00f9088118c947a05734b6aad3570d39e96bfbb0
Yangyu Chen <cyy@cyyself.name>
2020-10-31 00:52:52 +0800

    fixed page number

--- print_server/config.php
+++ print_server/config.php
@@ -1,6 +1,6 @@
 <?php
 	$GLOBALS['print_server'] = array(
 		'access_key'=>'your_key',
-		'pandoc_args'=>'--listings -H listings-setup.tex -V geometry:"left=1cm, top=1cm, right=1cm, bottom=1cm" -V fontsize=12pt --pdf-engine=xelatex'
+		'pandoc_args'=>'--listings -H listings-setup.tex -V geometry:"left=1cm, top=1cm, right=1cm, bottom=2cm" -V fontsize=12pt --pdf-engine=xelatex'
 	);
 ?>
